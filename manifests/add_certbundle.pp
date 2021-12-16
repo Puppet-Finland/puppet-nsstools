3e17b98e0e732b640c82e95427ac4cfcaf959e72
Kibahop <petri.lammi@puppeteers.net>
2021-12-16 21:13:00 +0200

    Make extracting certs from a bundle possible
    Signed-off-by: Kibahop <petri.lammi@puppeteers.net>

---
+++ manifests/add_certbundle.pp
@@ -0,0 +1,61 @@
+# Loads a certificate bundle into an NSS database.
+#
+# Parameters:
+#   $certdir    - required - defaults to $title
+#   $bundlefile - required - path to certificate bundle in PEM format
+#   $trustargs  - optional - defaults to 'CT,C,C'
+#   $temppath   - optional - temporary path to extract the certificates. Defaults to '/tmp/' 
+# 
+# Actions:
+#   loads certificate and key into the NSS database.
+#
+# Requires:
+#   $certdir
+#   $cert
+#
+# Sample Usage:
+#
+#   nsstools::add_certbundle { 'mybundle':
+#     bundlefile => '/tmp/mybundle.pem,
+#     certdir  => '/etc/pki/foo'
+#   }
+#
+#
+define nsstools::add_certbundle(
+  Stdlib::Absolutepath $bundlefile,
+  Stdlib::Absolutepath $certdir,
+  Stdlib::Absolutepath $temppath = '/tmp/',
+  String $trustargs = 'CT,C,C',
+) {
+  include nsstools
+
+  file { '/root/certextract':
+    ensure  => 'present',
+    content => epp('nsstools/certextract.epp', {
+      'bundlefile' => $bundlefile,
+      'certdir'    => $certdir,
+      'temppath'   => $temppath,
+      'trustargs'  => $trustargs,
+    }),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0700',
+  }
+
+  file { 'monitor bundle file changes':
+    ensure => 'file',
+    path   => $bundlefile,
+    notify => Exec["add_certbundle_${title}"],
+  }
+
+  exec { "add_certbundle_${title}":
+    command     => '/root/certextract',
+    logoutput   => true,
+    require     => [
+      Nsstools::Create[$certdir],
+      Class['nsstools'],
+      File['/root/certextract'],
+    ],
+    refreshonly => true,
+  }
+}
